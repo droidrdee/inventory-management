ea2629cb4f59dd927b6eec21bae7c4fbd885f47a
droidrdee <deepak.yourdomain@gmail.com>
2025-03-05 20:10:45 +0530

    Articles app

--- inventory_management/views.py
+++ inventory_management/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from articles.models import Article
+import random
+
 
 def test(request):
-    return HttpResponse("Hello Deepak")
+
+    article_obj = Article.objects.get(id=random.randint(1,3))
+    string = f"""
+    <h1> Title : {article_obj.title} Random : {random.randint(1000, 563256532)}</h1>
+    <h3>Content : {article_obj.content}</h3>
+    """
+    return HttpResponse(string)
